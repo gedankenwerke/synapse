cc1fdbd6162e44850f186ea249f2e045677e525b
EqrixZ <s6606022630049@email.kmutnb.ac.th>
2026-05-15 14:04:27 +0700

    Type.ts added

---
+++ services/tenant_roles/ternantRole.type.ts
@@ -0,0 +1,7 @@
+export type TenantRole = {
+    ID: string;
+    TenantID: string;
+    Name: string;
+    CreatedAt: string;
+    UpdatedAt: string;
+}
---
+++ services/tenant_user/tenantUser.types.ts
@@ -0,0 +1,9 @@
+export type TenantUser = {
+    ID: string;
+    TenantID: string;
+    TenantRoleID: string;
+    UserID: string;
+    Name: string;
+    CreatedAt: string;
+    UpdatedAt: string;
+}
